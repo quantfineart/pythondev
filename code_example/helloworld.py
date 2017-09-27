c542b0e7e7660fb419f4a3a335fa19c666a01822
Kapil Shukla <shukla.kapil@gmail.com>
2017-09-27 22:54:28 +1000

    creation of code_example directory first hello world program created

---
+++ code_example/helloworld.py
@@ -0,0 +1,7 @@
+"""
+starter program to display the simple hello world msg on the console.
+"""
+import sys
+
+#slight change of the plan and i am going to say Hello to the person name passed in the arugment
+print("Hello {0}".format(sys.argv[1]))
