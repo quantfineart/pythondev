69cf37c205818c7239883ceead3911727c68e746
kapil shukla <shukla.kapil@gmail.com>
2017-09-29 22:43:12 +1000

    improvement to code as per PEP8 and run msg for demo_datatypes

--- code_example/demo_datatypes.py
+++ code_example/demo_datatypes.py
@@ -1,10 +1,16 @@
 """
 Demo representation of native data structures available in python
-Even though Python is dynamically typed language its a strictly typed language i.e
-ever object or data structure is categorized as a specific type. This type also decides
-the operations which can be performed on that object. We will be covering:
-int, float,str, list, boolean,dictionary, set and Tuples
+
 """
 
 
+def run():
+    print("Even though Python is dynamically typed language its a strictly typed language "
+          "i.e ever object or data structure is categorized as a specific type. "
+          "This type also decides  the operations which can be performed on that object. We will be covering: "
+          " int, float,str, list, boolean,dictionary, set and Tuples")
+
+
+if __name__ == "__main__":
+    run()
 
--- code_example/helloworld.py
+++ code_example/helloworld.py
@@ -3,5 +3,5 @@ starter program to display the simple hello world msg on the console.
 """
 import sys
 
-#slight change of the plan and i am going to say Hello to the person name passed in the arugment
+# slight change of the plan and i am going to say Hello to the person name passed in the arugment
 print("Hello {0}".format(sys.argv[1]))
