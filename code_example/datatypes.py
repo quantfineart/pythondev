5055453d538ae9972e4882f1388d71735c6f248a
Kapil Shukla <shukla.kapil@gmail.com>
2017-09-29 18:19:22 +1000

    Update docstring for the module

---
+++ code_example/datatypes.py
@@ -0,0 +1,10 @@
+"""
+Demo representation of native data structures available in python
+Even though Python is dynamically typed language its a strictly typed language i.e
+ever object or data structure is categorized as a specific type. This type also decides
+the operations which can be performed on that object. We will be covering:
+int, float,str, list, boolean,dictionary, set and Tuples
+"""
+
+
+
